ed461ddcc00bfe9572794d6b84d69abc3efc6ac6
seva <szhukov@icerockdev.com>
2019-05-18 22:22:47 +0700

    sign up activity

---
+++ app/src/main/java/com/urbanist/music/feature/sign_up/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.urbanist.music.feature.sign_up
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.urbanist.music.R
+
+class SignUpActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
