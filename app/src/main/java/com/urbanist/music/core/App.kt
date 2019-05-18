6273e630968c8c124b8a2c4c0de0319d2d820c48
latygin <denisss2221@gmail.com>
2019-05-18 23:44:53 +0700

    MapToFragment

--- app/src/main/java/com/urbanist/music/core/App.kt
+++ app/src/main/java/com/urbanist/music/core/App.kt
@@ -1,10 +1,21 @@
 package com.urbanist.music.core
 
+import androidx.fragment.app.Fragment
 import com.urbanist.music.core.dagger.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
 
-class App : DaggerApplication() {
+
+class App : DaggerApplication(), HasSupportFragmentInjector {
+
+    @Inject
+    lateinit var mFragmentInjector: DispatchingAndroidInjector<Fragment>
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> =
+        mFragmentInjector
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerApplicationComponent
