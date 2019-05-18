d3f159593954afad4a1ade201a37d0c46c79a41b
seva <szhukov@icerockdev.com>
2019-05-19 01:28:37 +0700

    sign up

---
+++ app/src/main/java/com/urbanist/music/core/domain/Busker.kt
@@ -0,0 +1,8 @@
+package com.urbanist.music.core.domain
+
+class Busker(
+    val name: String?,
+    val props: String?,
+    instruments: MutableSet<String>?,
+    genres: MutableSet<String>?
+)
--- app/src/main/java/com/urbanist/music/feature/sign_up/SignUpActivity.kt
+++ app/src/main/java/com/urbanist/music/feature/sign_up/SignUpActivity.kt
@@ -1,12 +1,40 @@
 package com.urbanist.music.feature.sign_up
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.urbanist.music.R
+import com.urbanist.music.core.presentation.BaseActivity
+import com.urbanist.music.databinding.ActivitySignUpBinding
+import javax.inject.Inject
 
-class SignUpActivity: AppCompatActivity() {
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>() {
+    override val layoutId = R.layout.activity_sign_up
+
+    @Inject
+    lateinit var viewModel: SignUpViewModel
+
+    override fun initBinding() {
+        requireBinding().viewModel = viewModel
+    }
+
+    override fun initViewModel(state: Bundle?) {
+        viewModel.onBind()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+    }
+
+    private val eventsListener: SignUpViewModel.EventsListener =
+        object : SignUpViewModel.EventsListener {
+            override fun showMessage(message: String) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.onUnbind()
     }
 }
