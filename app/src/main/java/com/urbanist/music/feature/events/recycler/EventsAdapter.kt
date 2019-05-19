9167ff38461654b58af86d558e1729a83b5b1439
latygin <denisss2221@gmail.com>
2019-05-19 13:05:00 +0700

    ыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыы

--- app/src/main/java/com/urbanist/music/feature/events/recycler/EventsAdapter.kt
+++ app/src/main/java/com/urbanist/music/feature/events/recycler/EventsAdapter.kt
@@ -1,19 +1,15 @@
 package com.urbanist.music.feature.events.recycler
 
-import android.location.Location
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
-import com.google.type.LatLng
 import com.urbanist.music.R
 import com.urbanist.music.databinding.ItemEventBinding
 import com.urbanist.music.feature.map.domain.Event
 
-class EventsAdapter constructor(
-    private var currentPosition: LatLng
-) : RecyclerView.Adapter<EventsListViewHolder>() {
+class EventsAdapter : RecyclerView.Adapter<EventsListViewHolder>() {
 
 
     var eventList: List<Event> = arrayListOf()
@@ -21,30 +17,8 @@ class EventsAdapter constructor(
     val onEventsClickEvent = MutableLiveData<Event>()
 
     fun updateEvents(eventList: List<Event>) {
-        sortByDistance()
         this.eventList = eventList
         notifyDataSetChanged()
-    }
-
-    fun updateCurrentPosition(latLng: LatLng) {
-        currentPosition = latLng
-    }
-
-    private fun sortByDistance() {
-
-        eventList.sortedBy {
-
-            val location = Location("")
-            location.latitude = currentPosition.latitude
-            location.longitude = currentPosition.longitude
-
-            val location2 = Location("")
-            location2.latitude = it.latitude
-            location2.longitude = it.longitude
-
-            it.dist = location.distanceTo(location2)
-            location.distanceTo(location2)
-        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventsListViewHolder {
